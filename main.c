609ddfbd1235e7758fc35cf723e60fdc8e58dfbf
spotgalaxy <spotgalaxy_23@163.com>
2026-02-12 01:13:50 +0800

    完成卖家目录，卖家上架商品功能。

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <Windows.h>
 #include <stdbool.h>
+#include <time.h>
 
 typedef struct {
 	int year;
@@ -15,7 +16,7 @@ typedef struct {
 typedef struct {
 	char id[7];
 	char name[10];
-	float price;
+	double price;
 	char description[30];
 	char sellerId[7];
 	Data data;
@@ -25,7 +26,7 @@ typedef struct {
 typedef struct {
 	char orderId[7];
 	char goodsId[7];
-	float sum;
+	double sum;
 	Data data;
 	char sellerId[7];
 	char buyerId[7];
@@ -55,10 +56,15 @@ void checkAllOrder();
 void checkAllUser();
 void delUser();
 void delistGoods();
-void privateMenu();
+void privateMenu(char* UserId);
 void buyer();
-void seller();
+void seller(char* UserId);
 void privateInfocontrol();
+void publishGoods(char* UserId);
+void checkPublishedGoods();
+void changeGoodsInfo();
+void delistOwnGoods();
+void checkHistoryOrder();
 
 
 int main() {
@@ -91,6 +97,8 @@ int main() {
 			puts("\n\n感谢您的使用，下次再见，拜拜！");
 			Sleep(2000);
 			exit(0);
+		default:
+			puts("无效的选择，请重新输入。\n\n");
 		}
 	}
 
@@ -104,6 +112,7 @@ void logIn() {
 	char name[50] = { 0 };
 	char buffer[150] = { 0 };
 	char password[11] = { 0 };
+	char UserId[10] = { 0 };
 
 	Users user = { 0 };
 
@@ -137,6 +146,7 @@ void logIn() {
 
 		if (strcmp(name, user.name) == 0) {
 			isFound = true;
+			memcpy(UserId, user.Uid, 7);
 			break;
 		}
 	}
@@ -153,7 +163,7 @@ void logIn() {
 			Sleep(2000);
 			system("cls");
 			
-			privateMenu();
+			privateMenu(UserId);
 		}
 		else {
 			if (cnt) {
@@ -311,13 +321,13 @@ void adminLogIn() {
 
 void manage() {
 
-	puts("====================================================================================");
-	puts("1.查看所有商品 2.搜索商品 3.查看所有订单 4.查看所有用户 5.删除用户 6.下架商品 7.注销");
-	puts("====================================================================================\n");
-
 	int choice;
 
 	while (1) {
+
+		puts("====================================================================================");
+		puts("1.查看所有商品 2.搜索商品 3.查看所有订单 4.查看所有用户 5.删除用户 6.下架商品 7.注销");
+		puts("====================================================================================\n");
 
 		printf("选择您的操作 : ");
 
@@ -348,6 +358,8 @@ void manage() {
 			Sleep(1500);
 			system("cls");
 			return;
+		default:
+			puts("无效的选择，请重新输入。\n\n");
 		}
 	}
 }
@@ -378,7 +390,7 @@ void checkAllGoods() {
 		goods[strcspn(goods, "\n")] = '\0';
 
 		int ret = sscanf(goods,
-			"%6[^,],%9[^,],%f,%29[^,],%6[^,],%d-%d-%d,%6[^\n]",
+			"%6[^,],%9[^,],%lf,%29[^,],%6[^,],%d-%d-%d,%6[^\n]",
 			good.id,
 			good.name,
 			&good.price,
@@ -440,7 +452,7 @@ void searchGoods() {
 	while (fgets(goods, sizeof(goods), fp)) {
 
 		sscanf(goods,
-			"%6[^,],%9[^,],%f,%29[^,],%6[^,],%d-%d-%d,%6[^\n]",
+			"%6[^,],%9[^,],%lf,%29[^,],%6[^,],%d-%d-%d,%6[^\n]",
 			good.id,
 			good.name,
 			&good.price,
@@ -520,7 +532,7 @@ void checkAllOrder() {
 		orders[strcspn(orders, "\n")] = '\0';
 
 		int ret = sscanf(orders,
-			"%6[^,],%6[^,],%f,%d-%d-%d,%6[^,],%6[^\n]",
+			"%6[^,],%6[^,],%lf,%d-%d-%d,%6[^,],%6[^\n]",
 			order.orderId,
 			order.goodsId,
 			&order.sum,
@@ -617,14 +629,15 @@ void delistGoods() {
 }
 
 
-void privateMenu() {
-	puts("================================================");
-	puts("1.注销登录 2.我是买家 3.我是卖家 4.个人信息管理");
-	puts("================================================\n\n");
+void privateMenu(char* UserId) {
 
 	int choice;
 
 	while (1) {
+		puts("================================================");
+		puts("1.注销登录 2.我是买家 3.我是卖家 4.个人信息管理");
+		puts("================================================\n\n");
+
 		printf("选择您的操作 : ");
 
 		scanf("%d", &choice);
@@ -641,9 +654,10 @@ void privateMenu() {
 			break;
 		case 3:
 			system("cls");
-			seller();
+			seller(UserId);
 			break;
 		case 4:
+			system("cls");
 			privateInfocontrol();
 			break;
 		default:
@@ -657,14 +671,164 @@ void buyer() {
 
 }
 
-void seller() {
-	puts("====================================================================================");
-	puts("1.发布商品 2.查看发布商品 3.修改商品信息 4.下架商品 5.查看历史订单 6.返回用户主界面");
-	puts("====================================================================================\n\n");
+void seller(char* UserId) {
 
+	int choice;
 
+	while (1) {
+		puts("====================================================================================");
+		puts("1.发布商品 2.查看发布商品 3.修改商品信息 4.下架商品 5.查看历史订单 6.返回用户主界面");
+		puts("====================================================================================\n\n");
+
+		printf("选择您的操作 : ");
+
+		scanf("%d", &choice);
+
+		switch (choice) {
+		case 1:
+			publishGoods(UserId);
+
+			Sleep(2000);
+			system("cls");
+			break;
+		case 2:
+			checkPublishedGoods();
+			break;
+		case 3:
+			changeGoodsInfo();
+			break;
+		case 4:
+			delistOwnGoods();
+			break;
+		case 5:
+			checkHistoryOrder();
+			break;
+		case 6:
+			puts("正在返回用户主界面，请稍后...");
+			Sleep(2000);
+			system("cls");
+			return;
+		default:
+			puts("无效的选择，请重新输入。\n\n");
+		}
+	}
 }
 
 void privateInfocontrol() {
 
 }
+
+void publishGoods(char* UserId) {
+	Goods newGood = { 0 };
+
+	char buffer[150] = { 0 };
+
+	int id = 0;
+
+	Goods good = { 0 };
+
+	time_t raw_time;
+	time(&raw_time);
+
+	struct tm* time_info = localtime(&raw_time);
+
+	while (1) {
+
+		printf("请输入商品名称: ");
+		scanf("%s", newGood.name);
+
+		printf("请输入商品价格: ");
+		scanf("%lf", &newGood.price);
+
+		printf("请输入商品描述: ");
+		scanf("%s", newGood.description);
+		puts("\n");
+
+		puts("请确认商品信息无误.\n");
+
+		puts("*********************\n");
+		printf("商品名称: %s\n", newGood.name);
+		printf("商品价格: %.2lf\n", newGood.price);
+		printf("商品描述: %s\n", newGood.description);
+		puts("\n*********************\n\n");
+
+		char coe;
+		printf("您确认要发布该商品吗(y/n): ");
+		scanf(" %c", &coe);
+		puts("");
+		
+
+		if ('y' == coe) {
+			break;
+		}
+	}
+
+	FILE* fp = fopen("goodsList.txt", "a+");
+
+	if (!fp) {
+
+		perror("open failed");
+		return;
+	}
+
+	fgets(buffer, sizeof(buffer), fp);
+
+	while (fgets(buffer, sizeof(buffer), fp)) {
+
+		buffer[strcspn(buffer, "\n")] = '\0';
+
+		sscanf(buffer, "%6[^,],%9[^,],%lf,%29[^,],%6[^,],%d-%d-%d,%6[^\n]",
+			good.id,
+			good.name,
+			&good.price,
+			good.description,
+			good.sellerId,
+			&good.data.year,
+			&good.data.month,
+			&good.data.day,
+			good.statu
+		);
+
+		char mtid[7] = { 0 };
+		memcpy(mtid, good.id + 1, 5);
+
+		char* end;
+
+		id = (int)strtol(mtid, &end, NULL);
+	}
+
+	sprintf(newGood.id, "M%05d", id + 1);
+
+	memcpy(newGood.statu, "销售中", 7);
+
+	newGood.data.year = time_info->tm_year + 1900;
+	newGood.data.month = time_info->tm_mon + 1;
+	newGood.data.day = time_info->tm_mday;
+
+	memcpy(newGood.sellerId, UserId, 7);
+
+	fprintf(fp, "%s,%s,%.2lf,%s,%s,%04d-%02d-%02d,%s\n",
+		newGood.id,
+		newGood.name,
+		newGood.price,
+		newGood.description,
+		newGood.sellerId,
+		newGood.data.year,
+		newGood.data.month,
+		newGood.data.day,
+		newGood.statu
+	);
+
+	puts("发布中，请稍后...\n\n");
+	Sleep(1500);
+
+	puts("发布商品成功.");
+
+	fclose(fp);
+
+}
+
+void checkPublishedGoods() {}
+void changeGoodsInfo() {}
+void delistOwnGoods() {}
+void checkHistoryOrder() {}
